51818d7e8fe28793ae358f6cf8becd19174aeb05
Alexander Soare <alexander@co.bot>
2025-11-03 11:50:39 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Alex's setup for locking in.
+
+Just run `bash setup.sh`
---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+# Custom bash setup.
+echo "⚙️ Installing custom bash setup"
+cp files/bashrc_custom.sh ~/.bashrc_custom
+echo "source ~/.bashrc_custom.sh" >> ~/.bashrc.sh
+echo "✅ Done."
+
+# Check if git is installed, and if not, install it.
+if ! command -v git &> /dev/null
+then
+    echo "git could not be found, installing git..."
+    sudo apt update
+    sudo apt install -y git
+    echo "✅ Done."
+fi
+
+# VSCode settings
+echo "⚙️ Installing VSCode setup"
+mkdir -p ~/.config/Code/User
+cp files/vscode/keybindings.json ~/.config/Code/User
+cp files/vscode/settings.json ~/.config/Code/User
+echo "✅ Done."
+
+# Fuzzy finder. Only do this if ~/.fzf does not already exist.
+if [ -d ~/.fzf ]; then
+    echo "~/.fzf already exists, skipping fzf installation."
+else
+    echo "⚙️ Installing fzf..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    bash ~/.fzf/install --all
+    echo "✅ Done."
+fi
+ 
+# XCompose
+# TODO: There's another step to make this work properly
+echo "⚙️ Installing XCompose setup"
+cp files/XCompose ~/.XCompose
+echo "✅ Done."
+
+source ~/.bashrc
+source ~/.xsessionrc
